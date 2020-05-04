a3ecb0d422be31ac0affa7ab05e79887b2e7e27a
satish Agarwal <satagarw@microsoft.com>
2020-05-04 23:31:08 +0530

    adding fontawesome and empty container for one module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './home.svg';
 import './App.css';
+import Messages from './messages';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Messages
       </header>
+      <section className="App-section">
+        <Messages />
+      </section>
+      <footer className="App-footer">
+        <i class="fas fa-home"></i>
+        <i class="fas fa-search"></i>
+        <i class="fas fa-bell"></i>
+        <i class="fas fa-envelope"></i>
+      </footer>
     </div>
   );
 }
